b5d369a22b284ddfa5c7254af416fc5c690598b4
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-12-22 00:15:18 +0100

    Improve visibility of UI prints

--- src/main/groovy/webappcraft/reporting/ui/UserInterface.groovy
+++ src/main/groovy/webappcraft/reporting/ui/UserInterface.groovy
@@ -5,12 +5,12 @@ class UserInterface {
     private final Scanner scanner = new Scanner(System.in)
 
     String askFor(String prompt) {
-        print "Enter ${prompt}: "
+        print "\n\tEnter ${prompt}: "
         return scanner.nextLine()
     }
 
     static void inform(String message) {
-        println message
+        println "\n\t> ${message}"
     }
 
 }
